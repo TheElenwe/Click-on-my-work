0ff3e240886dbc9476de312167468cb229fafc7d
unknown <you@example.com>
2020-12-02 23:00:56 +0200

    Second commit

---
+++ main.js
@@ -0,0 +1,14 @@
+let  count = 0, step = 1;
+
+let counter = setInterval(function() {
+
+    if (count<0 || count>10)
+
+     if  (count +=step *=-1) console.log("go");
+
+     else console.log("go")
+     console.log(count)
+
+    count +=step
+       
+}, 500);
