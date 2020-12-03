ecf9ac7e0fcaddf7f48f7a618f6d9877916f6028
TheElenwe <73945429+TheElenwe@users.noreply.github.com>
2020-12-03 15:31:26 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
-let  count = 0, step = 1;
+let count=0;
+let step=1;
 
-let counter = setInterval(function() {
+let counter=setInterval(function(){
+    
+    if (count<0 || count>12){
+        if (count +=step *=-1){
+            console.log("Counter has reached upper or lower limit")
+        }else{
+            console.log("Counter has reached upper or lower limit")
+        }
+        }
+        console.log(count)
+        
+        count +=step;
 
-    if (count<0 || count>12)
-
-     if  (count +=step *=-1) console.log("Counter has reached upper or lower limit");
-
-     else console.log("Counter has reached upper or lower limit")
-     console.log(count)
-
-    count +=step
-       
-}, 500);
-
+      },500)
 
